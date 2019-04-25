91bc1765b9548d26828b92fa237549bc59b489f4
avdalyanvaghinak <vaghinakavdalyan@gmail.com>
2019-04-25 13:26:58 +0400

    Facebook, Linkedin, Twitter and GitHub Registration

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
+
+use Auth;
+use File;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
+        $authUser = $this->findOrCreateUser($user, $provider);
+        Auth::login($authUser, true);
+        return redirect($this->redirectTo);
+    }
+
+    public function findOrCreateUser($user, $provider)
+    {
+        $authUser = User::where('provider_id', $user->id)->first();
+        if ($authUser) {
+            return $authUser;
+        }
+        $fileContents = file_get_contents($user->getAvatar());
+        File::put(public_path() . '/avatars/' . $user->getId() . ".jpg", $fileContents);
+        $picture =($user->getId() . ".jpg");
+        if ($provider=='github'){
+            $user->name='User';
+        }
+        return User::create([
+            'name' => $user->name,
+            'email'    => $user->email,
+            'avatar'   => $picture,
+            'provider' => $provider,
+            'provider_id' => $user->id,
+        ]);
+    }
+}
